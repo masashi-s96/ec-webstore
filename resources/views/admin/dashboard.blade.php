568bfa5f592b0645f67d30887d2d58d6ef9cb9f4
曽根将史 <sonemasashi@sonemasashinoMacBook-Pro.local>
2021-08-19 19:29:03 +0900

    initial commit

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+
+
+  <!-- Scroll to top -->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
---
+++ resources/views/admin/product/index.blade.php
@@ -0,0 +1,45 @@
+@section('content')
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 ml-4 text-gray-800">Product</h1>
+            <ol class="breadcrumb">
+              <li class="breadcrumb-item"><a href="./">Home</a></li>
+              <li class="breadcrumb-item active" aria-current="page">Product</li>
+            </ol>
+    </div>
+    <!-- Datatables -->
+    <div class="col-lg-12">
+        <div class="card mb-4">
+          <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables</h6>
+          </div>
+          <div class="table-responsive p-3">
+            <table class="table align-items-center table-flush" id="dataTable">
+              <thead class="thead-light">
+                <tr>
+                  <th>Name</th>
+                  <th>Position</th>
+                  <th>Office</th>
+                  <th>Age</th>
+                  <th>Start date</th>
+                  <th>Salary</th>
+                </tr>
+              </thead>
+              
+              <tbody>
+                <tr>
+                  <td>Tiger Nixon</td>
+                  <td>System Architect</td>
+                  <td>Edinburgh</td>
+                  <td>61</td>
+                  <td>2011/04/25</td>
+                  <td>$320,800</td>
+                </tr>
+                  
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+
+@endsection
